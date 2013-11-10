487e7ea0dfcfd5ba1353fb529050244360d9d93d
Wil Gieseler <wilgieseler@me.com>
2013-11-10 14:21:13 -0800

    Initial Commit

---
+++ Polygun/PGDocument.h
@@ -0,0 +1,13 @@
+//
+//  PGDocument.h
+//  Polygun
+//
+//  Created by Wil Gieseler on 11/10/13.
+//  Copyright (c) 2013 Wil Gieseler. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PGDocument : NSDocument
+
+@end
---
+++ Polygun/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Polygun
+//
+//  Created by Wil Gieseler on 11/10/13.
+//  Copyright (c) 2013 Wil Gieseler. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
